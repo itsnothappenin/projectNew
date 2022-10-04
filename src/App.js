575b4753c62f4de777e5f5a937de54f18fdab1ce
aytanmustafayeva <aytandc@gmail.com>
2022-10-04 17:41:43 +0400

    news page bugfix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home';
-import Eventspage from './pages/Eventspage/Eventspage';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Eventcard from './components/Main/Eventspage/Events/Eventcard/Eventcard';
-import EventDetail from './pages/Eventspage/Detail/EventDetail';
-
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Eventspage from "./pages/Eventspage/Eventspage";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Eventcard from "./components/Main/Eventspage/Events/Eventcard/Eventcard";
+import EventDetail from "./pages/Eventspage/Detail/EventDetail";
+import News from "./pages/News/News";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/events" element={<Eventspage />} />
-            <Route path="/events/:id" element={<EventDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Eventspage />} />
+          <Route path="/events/:id" element={<EventDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
-
     </div>
   );
 }
